c59eb740e4fae4fd2457a532415dc94485ffee54
rkvrpl <rkvrpl@icloud.com>
2024-05-19 17:17:28 +0300

    have problem with card

--- src/assets/components/Card/CardParent/CardParent.jsx
+++ src/assets/components/Card/CardParent/CardParent.jsx
@@ -2,9 +2,18 @@ import styles from './CardParent.module.css'
 import { useState } from 'react'
 import words from '../../../data/data.json'
 import CardOfWord from '../CardOfWord'
+// import WordContext from '../../../Context/WordContext'
+// import { useContext, useEffect } from 'react'
+
 
 const CardParent = (props) => {
 	
+	// const { words, fetchWords } =
+    // useContext(WordContext)
+
+	// useEffect(() => {
+	// 	fetchWords()
+	// }, [])
 	const [value, setValue] = useState(0)
 
 	const handleSetValue = () =>{
